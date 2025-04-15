591043d58a03f16a8d7810b546092642e9b1bde1
vickyymosafan <mvickymosafan@gmail.com>
2025-04-15 20:49:44 +0700

    build: configure Vercel deployment and build output directory

--- vite.config.ts
+++ vite.config.ts
@@ -23,4 +23,8 @@ export default defineConfig({
             'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
         },
     },
+    build: {
+        outDir: 'build',
+        emptyOutDir: true,
+    },
 });
